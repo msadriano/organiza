9872b81f7feeef48465c48329491130fb8bc97b4
Adriano de M. Sousa <adrianomsousa@gmail.com>
2026-03-26 19:22:01 -0300

    feat: atualização de classe e estilos e desenvolvimento da app page (página principal)

--- organiza-web/app/(app)/page.tsx
+++ organiza-web/app/(app)/page.tsx
@@ -1,7 +1,11 @@
+import ListView from "./components/ListView";
+import TaskActionsWrapper from "./components/TaskActionsWrapper";
+
 export default function DashboardPage() {
   return (
-    <div>
-      <h1>Dashboard</h1>
+    <div className="flex-1 p-4 bg-background md:py-6 md:px-8 space-y-4">
+      <TaskActionsWrapper />
+      <ListView />
     </div>
   );
 }
