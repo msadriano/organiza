8bf33787deff9cdc7b1b6f1b1a297a37b36059b2
Adriano de M. Sousa <adrianomsousa@gmail.com>
2026-03-30 16:31:28 -0300

    refactor: alteração de cofiguração do cors e delete de 2 arquivos que chamavam SKD da anthropic

--- organiza-api/src/app.ts
+++ organiza-api/src/app.ts
@@ -19,7 +19,7 @@ app.use(limiter);
 app.use(helmet());
 app.use(
   cors({
-    origin: "http://localhost:3001",
+    origin: "*",
     credentials: true,
   }),
 );
--- organiza-api/src/lib/anthropic.ts
+++
@@ -1,5 +0,0 @@
-import Anthropic from "@anthropic-ai/sdk";
-
-const iaClient = new Anthropic({ apiKey: process.env.ANTHROPIC_API_KEY });
-
-export { iaClient };
